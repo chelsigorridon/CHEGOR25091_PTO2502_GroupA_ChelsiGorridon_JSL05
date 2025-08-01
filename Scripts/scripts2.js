8c6ba6872e883e4b306fc324bb5afbbc34845f2e
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-08-01 21:39:59 +0200

    Added new code into new script

--- Scripts/scripts2.js
+++ Scripts/scripts2.js
@@ -1,17 +1,23 @@
 
-let tasks = [];
+const STORAGE_KEY = "my-task-list";
+
 
 function loadTasks() {
-  const storedTasks = localStorage.getItem("tasks");
-
-  if (storedTasks) {
-    tasks = JSON.parse(storedTasks);
-  } else {
-    tasks = defaultTasks;
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }
+  const tasksJSON = localStorage.getItem(STORAGE_KEY);
+  return tasksJSON ? JSON.parse(tasksJSON) : [];
 }
 
+function saveTasks(tasks) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+}
 
-localStorage.setItem("key", JSON.stringify(data));
-let value = JSON.parse(localStorage.getItem("key"));
+function generateId() {
+  return Date.now();
+}
+
+document.getElementById("add-task-button").addEventListener("click", () => {
+  document.getElementById("task-title").value = "";
+  document.getElementById("task-desc").value = "";
+  document.getElementById("task-status").value = "todo";
+  document.getElementById("task-modal").showModal();
+});
